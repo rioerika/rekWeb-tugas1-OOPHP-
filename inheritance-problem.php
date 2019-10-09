74b9719c2816f3964ffc386c8655839d75995155
Rio Erika <rio_173040041@mail.unpas.ac.id>
2019-10-09 07:35:43 +0700

    Menambahkan fungsi getInfoLengkap pada file inheritance-problem.php

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -8,18 +8,36 @@ class Produk {
    public $judul  , 
    		  $penulis  ,
    		  $penerbit  ,
-   		  $harga ;
+   		  $harga ,
+   		  $jmlHalaman,
+   		  $waktuMain,
+   		  $tipe;
 
-   	public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0){
+   	public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0,$jmlHalaman =0 ,$waktuMain = 0, $tipe ){
    		$this->judul = $judul;
    		$this->penulis = $penulis;
    		$this->penerbit = $penerbit;
    		$this->harga = $harga;
+   		$this->jmlHalaman = $jmlHalaman;
+   		$this->waktuMain = $waktuMain;
+   		$this->tipe = $tipe;
    	}
 
   	public function getLabel(){
   		return "$this->penulis, $this->penerbit";
   	}
+
+  	public function getInfoLengkap(){
+		$str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+		if ($this->tipe == "Komik") {
+			$str.= " - {$this->jmlHalaman} Halaman.";
+		}elseif ($this->tipe == "Game") {
+			$str .= " ~ {$this->waktuMain} Jam.";
+		}
+
+		return $str;
+	}
 
 }
 
@@ -29,19 +47,16 @@ class CetakInfoProduk{
 		$str = " {$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})"; 
 		return $str;
 	}
+
+
 }
 
 
-$produk1 = new Produk("naruto", "Masasahi Kisimoto","Shonen Jump", 30000);
-$produk2 = new Produk("uncharted", "Neil Druckmann", "Sony Computer", 250000);
-$produk3 = new Produk("Dragon Ball");
+$produk1 = new Produk("naruto", "Masasahi Kisimoto","Shonen Jump", 30000, 100, 0, "Komik");
+$produk2 = new Produk("uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
-echo "Komik :" . $produk1->getLabel();
+echo $produk1->getInfoLengkap();
 echo "<br>";
-echo "Game :" . $produk2->getLabel();
-echo "<br>";
-
-$infoProduk = new CetakInfoProduk();
-echo $infoProduk-> cetak($produk1);
+echo $produk2->getInfoLengkap();
 
  ?>
