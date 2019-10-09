c9a0cc7dd1a87e5b07eb4d68927c6e72fb278ca4
Rio Erika <rio_173040041@mail.unpas.ac.id>
2019-10-09 09:05:10 +0700

    Memperbaiki pemanggilan contruct class parent dikelas child

--- overriding.php
+++ overriding.php
@@ -35,7 +35,7 @@ class Komik extends Produk{
 
   public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0, $jmlHalaman = 0){
 
-    parent::__construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0);
+    parent::__construct($judul, $penulis, $penerbit, $harga);
     $this->jmlHalaman = $jmlHalaman;
   }
 
@@ -50,7 +50,7 @@ class Komik extends Produk{
 
   public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0, $waktuMain = 0){
 
-     parent::__construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0);
+     parent::__construct($judul, $penulis, $penerbit, $harga);
      $this->waktuMain = $waktuMain;
 
   }
