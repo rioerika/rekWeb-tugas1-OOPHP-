ed68d2d6d2377b1abfb85483fb5e6176974cdf83
Rio Erika <rio_173040041@mail.unpas.ac.id>
2019-10-09 17:20:03 +0700

    Menambahkan property diskon(protected) pada class produk difile visibility.php

--- visibility.php
+++ visibility.php
@@ -5,24 +5,29 @@
 // Game
 
 class Produk {
-   public $judul  , 
-   		  $penulis  ,
-   		  $penerbit  ,
-   		  $harga;
+  public $judul, 
+  		  $penulis,
+  		  $penerbit;
+  protected $diskon = 0;
+  private $harga;
 
-   	public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0){
-   		$this->judul = $judul;
-   		$this->penulis = $penulis;
-   		$this->penerbit = $penerbit;
-   		$this->harga = $harga;
+  public function __construct($judul = "judul", $penulis="penulis", $penerbit= "penerbit", $harga=0){
+   	$this->judul = $judul;
+   	$this->penulis = $penulis;
+   	$this->penerbit = $penerbit;
+   	$this->harga = $harga;
    		
-   	}
+  }
 
-  	public function getLabel(){
-  		return "$this->penulis, $this->penerbit";
-  	}
+  public function getHarga(){
+    return$this->harga - ($this->harga * $this->diskon / 100);   
+  }
 
-  	public function getInfoProduk(){
+  public function getLabel(){
+  	return "$this->penulis, $this->penerbit";
+  }
+
+  public function getInfoProduk(){
 		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
 
 		return $str;
@@ -55,6 +60,10 @@ class Komik extends Produk{
 
   }
 
+  public function setDiskon( $diskon){
+    $this->diskon = $diskon;
+  }
+
 	public function getInfoProduk(){
 		$str = "Game : " . parent::getInfoProduk() . "~ {$this->waktuMain} Jam.";
 		return $str;
@@ -80,5 +89,7 @@ echo $produk1->getInfoProduk();
 echo "<br>";
 echo $produk2->getInfoProduk();
 echo "<hr>";
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
 
  ?>
